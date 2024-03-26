69f14469542a09007011998a4ccb3bc6aed5a6a7
dawud-shakir <100232620+dawud-shakir@users.noreply.github.com>
2024-03-25 23:47:03 -0600

    Update num2label.py

--- num2label.py
+++ num2label.py
@@ -1,25 +1,14 @@
-function class = num2label(number)
-
-
-switch number
-    case 1
-        class = "blues"; 
-    case 2 
-        class = "classical"; 
-    case 3 
-        class = "country";
-    case 4 
-        class = "disco";
-    case 5 
-        class = "hiphop";
-    case 6 
-        class = "jazz";
-    case 7 
-        class = "metal";
-    case 8
-        class = "pop";
-    case 9 
-        class = "reggae";
-    case 10 
-        class = "rock";
-end
+def num2label(number):
+    classes = {
+        1: "blues",
+        2: "classical",
+        3: "country",
+        4: "disco",
+        5: "hiphop",
+        6: "jazz",
+        7: "metal",
+        8: "pop",
+        9: "reggae",
+        10: "rock"
+    }
+    return classes.get(number, "Unknown")
