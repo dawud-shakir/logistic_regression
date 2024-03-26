79d622ce4656545bb603f8aa0241822cd062fdbd
dawud-shakir <100232620+dawud-shakir@users.noreply.github.com>
2024-03-25 23:45:55 -0600

    Create num2label.py

---
+++ num2label.py
@@ -0,0 +1,25 @@
+function class = num2label(number)
+
+
+switch number
+    case 1
+        class = "blues"; 
+    case 2 
+        class = "classical"; 
+    case 3 
+        class = "country";
+    case 4 
+        class = "disco";
+    case 5 
+        class = "hiphop";
+    case 6 
+        class = "jazz";
+    case 7 
+        class = "metal";
+    case 8
+        class = "pop";
+    case 9 
+        class = "reggae";
+    case 10 
+        class = "rock";
+end
